6ddf2e64a9da1c1f7e2e821c863a9782c4ef4713
Kevin Coulibaly <coulibaly.d.kevin@gmail.com>
2020-02-16 23:58:03 +0100

    Presentation improvements
    Starting game with 24 cards

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <h1>Memory</h1>
     <Board v-bind:cardCount="cardCount" />
   </div>
@@ -9,7 +8,7 @@
 <script>
 import Board from "./components/Board";
 
-const cardCount = 12;
+const cardCount = 24;
 
 export default {
   name: "App",
@@ -27,6 +26,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px 10em 0;
 }
 </style>
